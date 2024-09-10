26547b67db2fa9046884f029fa3342bb5ba79728
Charlie <charliefranzel@gmail.com>
2024-09-10 10:51:02 -0600

    submit assignment

--- average.py
+++ average.py
@@ -1,12 +1,9 @@
-"""
-Import the text_utils module you created and calculate the average number of
-words per line in a given text file. The text file that will be used to test
-this is the text file called "sample.txt" (located in the same directory as
-this exercise). The average number of words per line should be rounded down to
-the nearest integer.
+import argparse, text_utils        #imports the argparse and text ultilites libraries
+parser = argparse.ArgumentParser()    #allows arguments for filename
+parser.add_argument('filename', type=str, help="The name of the file")
+args = parser.parse_args()    #allows you to use argparse in other functions easily
 
-Print the average number of words per line in the text file in the following
-format:
-
-"Average words per line: [average]"
-"""
+def average_words():
+  average = text_utils.count_words(args.filename)/text_utils.count_lines(args.filename) #uses the other functions to devide them to get answer
+  return(average)
+average_words()            #calls function
--- text_utils.py
+++ text_utils.py
@@ -1,14 +1,38 @@
 def count_chars(text):
-    """
-    Count the number of characters in a text
-    """
+    file = open(text, 'r')  # opens file
+    data = file.read()      # reads file
+    file.close()            # closes file
+    return len(data)        # returns the character count
 
 def count_words(text):
-    """
-    Count the number of words in a text
-    """
+    number_of_words = 0 
+    file = open(text, 'r')     # opens file
+    data = file.read()
+    words = data.split()       # splits the words
+    number_of_words += len(words)  # counts the number of words
+    file.close()
+    return number_of_words
 
 def count_sentences(text):
-    """
-    Count the number of sentences in a text
-    """
+    file = open(text, 'r')
+    lines= list(file)
+    file_contents = file.read()
+    file.close()
+    words_all = 0
+    for line in lines: 
+        words_all = words_all + len(line.split())  # splits each line
+    full_stops = 0
+    for stop in lines:
+        full_stops = full_stops + len(stop.split('.')) # seperates each sentence and counts them
+    return full_stops
+
+def count_lines(text):
+  file = open(text, 'r')    
+  lines = file.readlines()     
+  linelist = []                # creates a list
+  for line in lines:
+    linelist.append((line))    # appends each line
+  file.close()                 
+  x = (int(len(linelist)))     # counts the lines
+  return x   
+
